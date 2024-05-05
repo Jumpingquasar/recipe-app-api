5977f5b6adea6061a5d998ca8c2a7c383adfc716
Arda Günal <102471766+Jumpingquasar@users.noreply.github.com>
2024-05-05 15:11:49 +0300

    Added PostgreSQL dependencies

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,6 +6,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the Calc Module"""
 
@@ -21,5 +22,3 @@ class CalcTests(SimpleTestCase):
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
-
-
